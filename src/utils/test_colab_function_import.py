80f94fa505fd4e919fcbecd4fcf956ec79b31002
giovannabrod <gbrod1000@gmail.com>
2025-05-10 15:32:04 +0200

    added function for testing modules import with libraries dependencies- part2

--- src/utils/test_colab_function_import.py
+++ src/utils/test_colab_function_import.py
@@ -1,7 +1,13 @@
 import torch
 
+from loguru import logger
+
 def say_hello():
     print("Hello")
 
+def say_hello_with_loguru():
+    logger.info("Hello")
+
 def is_cuda_available():
+    # colab has already torch installed (?)
     print("Cuda is available:", torch.cuda.is_available())
